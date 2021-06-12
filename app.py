4340ab2bf35172dfcd048d8c2f7f208b4df6d85d
Manasvi1999 <85783042+Manasvi1999@users.noreply.github.com>
2021-06-12 18:06:10 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ app = Flask(__name__)
 run_with_ngrok(app)
 
 # Model saved with Keras model.save()
-MODEL_PATH ="/vgg16.py"
+MODEL_PATH ="vgg16.py"
 
 # Load your trained model
 #from tensorflow.keras.applications.mobilenet import MobileNet
